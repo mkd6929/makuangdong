51e3c579dbeda60467c5f352a1ad9b978af52ad9
黑色乏味 <143991647+mkd6929@users.noreply.github.com>
2023-09-11 17:35:36 +0800

    Update streamlit_class.py

--- streamlit_class.py
+++ streamlit_class.py
@@ -252,7 +252,15 @@ def get_article(keyword):
     :return:
     """
     headers = {
-      "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36"
+      "referer": "https",
+      "sec-ch-ua": "Chromium;v=116, Not)A;Brand;v=24, Google Chrome;v=116",
+      "sec-ch-ua-mobile": "?0",
+      "sec-ch-ua-platform": "Windows",
+      "sec-fetch-dest": "empty",
+      "sec-fetch-mode": "cors",
+      "sec-fetch-site": "same-origin",
+      "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36",
+      "x-requested-with": "XMLHttpRequest"
     }
     url = 'https://so.ruiwen.com/res/best_kds/'
     params = {
